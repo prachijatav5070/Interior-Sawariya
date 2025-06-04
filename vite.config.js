f9befb6a6e0c4642f9ab7b18af1defd6a7c1272a
Manashvi-01 <manashvistaff.prachijatav@gmail.com>
2025-06-04 23:10:33 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// for SPA fallback
 export default defineConfig({
+base: "/Interior-Sawariya/",
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: './index.html'
+    }
+  }
+});
